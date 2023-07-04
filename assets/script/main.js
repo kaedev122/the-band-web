dfa0b72f368a2ee8c3e990eff3eab9076035056a
kaekaze <fongnguyen122@gmail.com>
2023-07-04 20:27:57 +0700

    responsive tablet

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,7 +3,7 @@ const buyBtns = document.querySelectorAll(".place-buy-btn");
 const closeBtns = document.querySelectorAll(".tickets-modal-close-js");
 const ticketsFormContainer = document.querySelector(".tickets-modal-form");
 const ticketsModal = document.querySelector(".js-tickets-modal")
-/////////////////////////
+
 for (const buyBtn of buyBtns) {
     buyBtn.addEventListener("click", showTicketsModal);
 }
@@ -16,7 +16,7 @@ ticketsFormContainer.addEventListener("click", closeTicketsModal);
 ticketsModal.addEventListener("click", function(event) {
     event.stopPropagation();
 })
-/////////////////
+
 function showTicketsModal() {
     ticketsForm.classList.remove("hidden");
 }
@@ -24,3 +24,32 @@ function showTicketsModal() {
 function closeTicketsModal() {
     ticketsForm.classList.add("hidden");
 }
+
+////////////////////////////////////////////////////////////////
+var header = document.getElementById("header");
+var mobileMenu = document.querySelector(".mobile-menu-bar");
+var headerHeight = header.clientHeight;
+
+mobileMenu.onclick = function() {
+    var isClosed = header.clientHeight === headerHeight;
+    if (isClosed) {
+        header.style.height = "auto";
+    }
+    else {
+        header.style.height = null;
+    }
+}
+
+////////////////////////////////////////////////////////////////
+var menuItems = document.querySelectorAll("#navbar li a[href*='#']");
+for (const menuItem of menuItems) {
+    var isParentMenu = menuItem.nextElementSibling && menuItem.nextElementSibling.classList.contains('subnavbar');
+    
+    menuItem.onclick = function() {
+        if (!isParentMenu) {
+            header.style.height = null;
+        console.log(menuItem.nextElementSibling)
+        }
+    }
+}
+////////////////////////////////////////////////////////////////
